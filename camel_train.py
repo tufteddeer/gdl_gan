2a4cebf6e4928b3a8bc89806012f79d08d0331a2
TuftedDeer <tufteddeer@disroot.org>
2021-11-16 13:07:01 +0100

    remove jupyter stuff

--- camel_train.py
+++ camel_train.py
@@ -1,22 +1,9 @@
-#%% md
-
-# GAN Training
-
-#%% md
-
-## imports
-
-#%%
-
 import os
 
 import matplotlib.pyplot as plt
 
 from GAN import GAN
 from loaders import load_safari
-
-
-#%%
 
 # run params
 SECTION = 'gan'
@@ -31,86 +18,56 @@ if not os.path.exists(RUN_FOLDER):
     os.mkdir(os.path.join(RUN_FOLDER, 'images'))
     os.mkdir(os.path.join(RUN_FOLDER, 'weights'))
 
-mode =  'build' #'load' #
-
-#%% md
-
-## data
-
-#%%
+mode = 'build'  # 'load' #
 
 x_train = load_safari()
 
-#%%
-
-x_train.shape
-
-#%%
-
-plt.imshow(x_train[200,:,:,0], cmap = 'gray')
+plt.imshow(x_train[200, :, :, 0], cmap='gray')
 plt.show()
 
-#%% md
-
-## architecture
-
-#%%
-
-gan = GAN(input_dim = (28,28,1)
-        , discriminator_conv_filters = [64,64,128,128]
-        , discriminator_conv_kernel_size = [5,5,5,5]
-        , discriminator_conv_strides = [2,2,2,1]
-        , discriminator_batch_norm_momentum = None
-        , discriminator_activation = 'relu'
-        , discriminator_dropout_rate = 0.4
-        , discriminator_learning_rate = 0.0008
-        , generator_initial_dense_layer_size = (7, 7, 64)
-        , generator_upsample = [2,2, 1, 1]
-        , generator_conv_filters = [128,64, 64,1]
-        , generator_conv_kernel_size = [5,5,5,5]
-        , generator_conv_strides = [1,1, 1, 1]
-        , generator_batch_norm_momentum = 0.9
-        , generator_activation = 'relu'
-        , generator_dropout_rate = None
-        , generator_learning_rate = 0.0004
-        , optimiser = 'rmsprop'
-        , z_dim = 100
-        )
+gan = GAN(input_dim=(28, 28, 1)
+          , discriminator_conv_filters=[64, 64, 128, 128]
+          , discriminator_conv_kernel_size=[5, 5, 5, 5]
+          , discriminator_conv_strides=[2, 2, 2, 1]
+          , discriminator_batch_norm_momentum=None
+          , discriminator_activation='relu'
+          , discriminator_dropout_rate=0.4
+          , discriminator_learning_rate=0.0008
+          , generator_initial_dense_layer_size=(7, 7, 64)
+          , generator_upsample=[2, 2, 1, 1]
+          , generator_conv_filters=[128, 64, 64, 1]
+          , generator_conv_kernel_size=[5, 5, 5, 5]
+          , generator_conv_strides=[1, 1, 1, 1]
+          , generator_batch_norm_momentum=0.9
+          , generator_activation='relu'
+          , generator_dropout_rate=None
+          , generator_learning_rate=0.0004
+          , optimiser='rmsprop'
+          , z_dim=100
+          )
 
 if mode == 'build':
     gan.save(RUN_FOLDER)
 else:
     gan.load_weights(os.path.join(RUN_FOLDER, 'weights/weights.h5'))
 
-#%%
-
 gan.discriminator.summary()
-
-#%%
 
 gan.generator.summary()
 
-#%% md
-
-## training
-
-#%%
+# training
 
 BATCH_SIZE = 64
-EPOCHS = 100#6000
+EPOCHS = 6000
 PRINT_EVERY_N_BATCHES = 5
-
-#%%
 
 gan.train(
     x_train
-    , batch_size = BATCH_SIZE
-    , epochs = EPOCHS
-    , run_folder = RUN_FOLDER
-    , print_every_n_batches = PRINT_EVERY_N_BATCHES
+    , batch_size=BATCH_SIZE
+    , epochs=EPOCHS
+    , run_folder=RUN_FOLDER
+    , print_every_n_batches=PRINT_EVERY_N_BATCHES
 )
-
-#%%
 
 fig = plt.figure()
 plt.plot([x[0] for x in gan.d_losses], color='black', linewidth=0.25)
@@ -127,9 +84,6 @@ plt.ylim(0, 2)
 
 plt.show()
 
-
-#%%
-
 fig = plt.figure()
 plt.plot([x[3] for x in gan.d_losses], color='black', linewidth=0.25)
 plt.plot([x[4] for x in gan.d_losses], color='green', linewidth=0.25)
@@ -142,7 +96,3 @@ plt.ylabel('accuracy', fontsize=16)
 plt.xlim(0, 2000)
 
 plt.show()
-
-#%%
-
-
