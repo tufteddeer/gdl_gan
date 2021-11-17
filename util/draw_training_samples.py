aa125d472af52f475b99dc91dc59c25e8164cd8c
TuftedDeer <tufteddeer@disroot.org>
2021-11-17 13:44:44 +0100

    add script to draw random training samples

---
+++ util/draw_training_samples.py
@@ -0,0 +1,20 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from loaders import load_safari
+
+# draw some random samples from the camel data
+if __name__ == '__main__':
+
+    images = load_safari("../data/camel/camel.npy")
+    np.random.shuffle(images)
+    rows = columns = 5
+    fig, axs = plt.subplots(rows, columns, figsize=(15, 15))
+    cnt = 0
+    for i in range(rows):
+        for j in range(columns):
+            axs[i, j].imshow((images[cnt, :, :, :]), cmap='gray_r')
+            axs[i, j].axis('off')
+            cnt += 1
+    plt.show()
+    fig.savefig("camel_samples.eps")
