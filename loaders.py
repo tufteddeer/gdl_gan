98d5794e8a18160e14c5b00d8785d52398e3ffaa
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2021-11-25 20:06:31 +0100

    Add load func for cifar dataset

--- loaders.py
+++ loaders.py
@@ -1,11 +1,10 @@
 import os
 import numpy as np
 from keras_preprocessing.image import ImageDataGenerator
+from tensorflow.keras.datasets import cifar100, cifar10
 
 
 def load_safari(path):
-
-
     print("Loading camel images from {}".format(path))
     data = np.load(path)
     # normalize data to range [-1, 1]
@@ -29,3 +28,20 @@ def load_celeb(data_name, image_size, batch_size):
                                            )
 
     return x_train
+
+
+def load_cifar(label, num):
+    if num == 10:
+        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    else:
+        (x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode='fine')
+
+    train_mask = [y[0] == label for y in y_train]
+    test_mask = [y[0] == label for y in y_test]
+
+    x_data = np.concatenate([x_train[train_mask], x_test[test_mask]])
+    y_data = np.concatenate([y_train[train_mask], y_test[test_mask]])
+
+    x_data = (x_data.astype('float32') - 127.5) / 127.5
+
+    return x_data, y_data
